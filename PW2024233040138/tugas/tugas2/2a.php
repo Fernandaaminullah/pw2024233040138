f422d676f756bec6490c1ff0cf02603019bbd089
Fernandaaminullah <147626548+Fernandaaminullah@users.noreply.github.com>
2024-02-28 10:09:03 +0700

    Add files via upload

---
+++ PW2024233040138/tugas/tugas2/2a.php
@@ -0,0 +1,17 @@
+<?php 
+$d = "Fernanda";
+$a = "Aminullah";
+
+for($n = 1; $n <= 100; $n++) {
+
+    if( $n % 3 === 0 & $n % 5 === 0 ) {
+    echo "$d $a <br>";
+    } else if ( $n % 3 === 0 ) {
+    echo "$d <br>";
+    } else if ( $n % 5 === 0 ) {
+    echo "$a <br>";
+    } else {
+    echo "$n <br>";
+    }
+}
+?>
